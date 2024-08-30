9ec9b9d0a99c7cf01d74e7e988d0c8227ba21ce7
awkwy <43932871+awkwy@users.noreply.github.com>
2024-08-30 15:59:49 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +7,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		kit: {		adapter: adapter({			fallback: '404.html'		}),		paths: {			base: process.argv.includes('dev') ? '' : process.env.BASE_PATHType 'string | undefined' is not assignable to type '"" | `/${string}` | undefined'.
-  Type 'string' is not assignable to type '"" | `/${string}` | undefined'.2322Type 'string | undefined' is not assignable to type '"" | `/${string}` | undefined'.
-  Type 'string' is not assignable to type '"" | `/${string}` | undefined'.		}	}};
+		adapter: adapter()
+	},
 
 	preprocess: [vitePreprocess({})]
 };
